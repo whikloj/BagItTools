e8800d61ac830f0e2ce45ad851ed95a93a47d18a
Jared Whiklo <jwhiklo@gmail.com>
2024-01-17 10:18:32 -0600

    Initial BagItProfile work

---
+++ src/Exceptions/ProfileException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace whikloj\BagItTools\Exceptions;
+
+/**
+ * Errors related to BagItProfiles.
+ * @author Jared Whiklo
+ * @since 5.0.0
+ */
+class ProfileException extends BagItException
+{
+}
